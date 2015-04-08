33b0d84ade8b58af878515c8d97a1f0229247815
Luc Awater <lucawater@gmail.com>
2015-04-08 13:40:07 +0200

    reinitialized scripts

--- content/themes/elements/content/text.php
+++ content/themes/elements/content/text.php
@@ -9,7 +9,7 @@ $text_align = get_sub_field( 'text_o_c_align' );
 $title = get_sub_field( 'text_c_title' );
 $content = preg_replace( '/<p>/', '<p class="is_aligned-' . $text_align . '">', get_sub_field( 'text_c_text' ) );
 ?>
-<section class="section section_text" id="<?php if( $text_menu == true ){ echo 'anchor-' . $i_anchor; }; ?>">
+<section class="section section_text has_anchor" id="<?php if( $text_menu == true ){ echo 'anchor-' . $i_anchor; }; ?>">
   <div class="section_content row <?php echo 'is_positioned-' . $text_pos; ?>">
     <h2 class="s-4 columns is_uppercase is_bold <?php echo 'is_aligned-' . $text_align; ?>">
       <?php echo $title; ?>
