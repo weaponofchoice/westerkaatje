42246fbd8a594e32dbd0d9fb3b76dc839bf985f8
Luc Awater <lucawater@gmail.com>
2015-06-09 09:59:50 +0200

    add text element

--- content/themes/elements/content/text.php
+++ content/themes/elements/content/text.php
@@ -1,21 +1,12 @@
 <?php
 // Options
-$text_o_menu = get_sub_field( 'text_o_menu' );
-
-$text_o_c_pos = get_sub_field( 'text_o_c_pos' );
-$text_o_c_align = get_sub_field( 'text_o_c_align' );
+$text_o_background = get_sub_field( 'text_o_background' );
 
 // Content
-$text_c_title = get_sub_field( 'text_c_title' );
-$text_c_text = preg_replace( '/<p>/', '<p class="is_aligned-' . $text_o_c_align . '">', get_sub_field( 'text_c_text' ) );
+$text_c_text = get_sub_field( 'text_c_text' );
 ?>
-<section class="section_text <?php if( $text_o_menu == true ){ echo 'has_anchor" id="anchor-' . $i_anchor; } else { echo '"'; }; ?>">
-  <div class="section_body row <?php echo 'is_positioned-' . $text_o_c_pos; ?>">
-    <h2 class="s-4 columns is_uppercase is_bold <?php echo 'is_aligned-' . $text_o_c_align; ?>">
-      <?php echo $text_c_title; ?>
-    </h2>
-    <div class="s-4 columns">
-      <?php echo $text_c_text; ?>
-    </div>
+<section class="section-text <?php if( $text_o_background == true ){ echo 'has-bg'; } ?>">
+  <div class="section-body">
+    <?php echo $text_c_text; ?>
   </div>
 </section>
--- content/themes/elements/page.php
+++ content/themes/elements/page.php
@@ -2,22 +2,20 @@
 get_header();
 
 // Loop into ACF groups
-if( have_rows('page') ): $i_anchor = 1; $i_par = 0;
+if( have_rows('page') ):
   while( have_rows('page') ): the_row();
     
-    // Hero section is placed before main
-    
     if( get_row_layout() == 'text' ):
-      include( locate_template('content/text.php') ); $i_anchor++;
+      include( locate_template('content/text.php') );
     elseif( get_row_layout() == 'image' ):
       include( locate_template('content/image.php') );
     elseif( get_row_layout() == 'grid_primary' ):
-      include( locate_template('content/gridPri.php') ); $i_anchor++;
+      include( locate_template('content/gridPri.php') );
     elseif( get_row_layout() == 'grid_secondary' ):
-      include( locate_template('content/gridSec.php') ); $i_anchor++;
+      include( locate_template('content/gridSec.php') );
     elseif( get_row_layout() == 'slider' ):
-      include( locate_template('content/slider.php') ); $i_anchor++;
-    elseif( get_row_layout() == 'parallax' ): $i_par++;
+      include( locate_template('content/slider.php') );
+    elseif( get_row_layout() == 'parallax' ):
       include( locate_template('content/parallax.php') );
     endif;
     
