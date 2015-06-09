622eaed536f81fafb821ec25f60772056180d2d4
Luc Awater <lucawater@gmail.com>
2015-06-09 09:41:42 +0200

    change backend menu items

--- content/themes/elements/includes/admin/acf-page.php
+++ content/themes/elements/includes/admin/acf-page.php
@@ -2,11 +2,11 @@
 // Creates ACF options pages
 if( function_exists('acf_add_options_page') ){
   
-  acf_add_options_page( array(
-    'page_title'  => 'Footer',
-    'menu_title'  => 'Footer',
-    'menu_slug'   => 'footer',
-    'redirect'    => false
-  ));
+  // acf_add_options_page( array(
+//     'page_title'  => 'Footer',
+//     'menu_title'  => 'Footer',
+//     'menu_slug'   => 'footer',
+//     'redirect'    => false
+//   ));
 }
 ?>
