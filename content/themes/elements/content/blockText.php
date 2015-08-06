81e4bddd0494fc9e0dc1779be61cdc60689953e7
Luc Awater <lucawater@gmail.com>
2015-08-06 10:02:27 +0200

    add blank target option to text block button

--- content/themes/elements/content/blockText.php
+++ content/themes/elements/content/blockText.php
@@ -5,16 +5,17 @@ $text = get_sub_field( 'blockText_c_text' );
 <section class="section-block text">
   <div class="section-body">
     <?php echo $text; ?>
-    
+
     <?php
     // Optional button
     if( have_rows('blockText_c_button') ):
       while( have_rows('blockText_c_button') ): the_row();
         $button_link = get_sub_field( 'link' );
         $button_text = get_sub_field( 'text' );
-        
-        echo '<a class="button" href="' . $button_link . '">' . $button_text . '</a>';
-        
+        $button_target = get_sub_field( 'target' );
+
+        echo '<a class="button" ' . ( ($button_target == true) ? 'target="_blank"' : "" ) . ' href="' . $button_link . '">' . $button_text . '</a>';
+
       endwhile;
     endif;
     ?>
