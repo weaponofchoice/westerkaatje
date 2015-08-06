4d932d5c92ae460f1ecd60f5d6395267157b601b
Luc Awater <lucawater@gmail.com>
2015-08-06 09:31:14 +0200

    add text color option to image block

--- content/themes/elements/content/blockImage.php
+++ content/themes/elements/content/blockImage.php
@@ -2,6 +2,9 @@
 // Content
 $images = get_sub_field( 'blockImage_c_images' );
 $text = get_sub_field( 'blockImage_c_text' );
+
+// Options
+$textColor = get_sub_field( 'blockImage_o_textColor' );
 ?>
 <section class="section-block image slider slider-auto">
   <ul class="slider-images">
@@ -9,5 +12,5 @@ $text = get_sub_field( 'blockImage_c_text' );
       <li style="background-image: url('<?php echo $image['sizes']['large']; ?>');"></li>
     <?php endforeach; ?>
   </ul>
-  <p><?php echo $text; ?></p>
+  <p class="is-<?php echo $textColor; ?>"><?php echo $text; ?></p>
 </section>
