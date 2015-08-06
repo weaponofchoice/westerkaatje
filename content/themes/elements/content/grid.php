17939dee13cce3585166de726aac23365b679ab0
Luc Awater <lucawater@gmail.com>
2015-08-06 10:20:20 +0200

    fix grid margins and paddings

--- content/themes/elements/content/grid.php
+++ content/themes/elements/content/grid.php
@@ -4,13 +4,13 @@ $images = get_sub_field( 'grid_c_images' );
 ?>
 <section class="section-grid row">
   <div class="section-body row">
-    <ul class="m-grid-4">
+    <ul class="m-grid-2 l-grid-4">
       <?php
       foreach( $images as $image ):
         $image = $image['sizes']['large'];
-        
+
         echo '<li><img src="' . $image . '" width="' . $image['width'] . '" height="' . $image['height'] . '"></li>';
-        
+
       endforeach;
       ?>
     </ul>
