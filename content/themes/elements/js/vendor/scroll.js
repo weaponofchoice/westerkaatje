312839c6fa11d76ab478e9f64c6b59659512880c
Luc Awater <lucawater@gmail.com>
2015-08-17 14:40:34 +0200

    Tweak scroll position

--- content/themes/elements/js/vendor/scroll.js
+++ content/themes/elements/js/vendor/scroll.js
@@ -5,7 +5,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 60
+          scrollTop: target.offset().top - 100
         }, 500);
         return false;
       }
